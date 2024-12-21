b29a923b865e814b91e17576c711de137dd50d5d
Fabian Stroh <fabian.stroh@gmail.com>
2024-12-21 01:50:22 +0100

    add a what is this section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,19 @@
 
 <template>
   <main>
-
+    <div class="main-page">
+      <h1>What is this?</h1>
+      <p>This is a helper tool for the <a href="https://www.pirategonzalezgames.com/">Beacon Tabletop RPG</a> by Pirate Gonzales Games. Specifically it lets you generate loot boxes, reroll slots, and hand-assemble specific loot box contents. It also lets you assemble specific or random setups of shops.</p>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.main-page {
+  padding-top: 50px;
+  max-width: 600px;
+  a:visited {
+    color: #5a7aab;
+  }
+}
+</style>
