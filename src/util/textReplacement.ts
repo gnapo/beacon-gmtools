cef98eacccb0e4c442311186448c5b98ab299f57
Fabian Stroh <fabian.stroh@gmail.com>
2024-12-21 19:48:44 +0100

    clean up

--- src/util/textReplacement.ts
+++ src/util/textReplacement.ts
@@ -1,6 +1,6 @@
 interface replacementAction {
   pattern: RegExp
-  replacer: (match, p1: string) => string
+  replacer: (match: string, p1: string) => string
 }
 
 
@@ -17,14 +17,14 @@ export const doReplacements = (initialString: string, replacements: replacementA
 
 export const replacePhaseNumberInTop = {
   pattern: /\((\d+)\)/g,
-  replacer: (match, number) => {
+  replacer: (match: string, number: string) => {
     return `<span class="phase-number">${number}</span>`
   }
 }
 
 export const putLinebreaksOnLists = {
   pattern: /\D (-) /g,
-  replacer: (match, group) => {
+  replacer: (match: string, group: string) => {
     return ` <br> ✦ `
   }
 }
@@ -32,31 +32,31 @@ export const putLinebreaksOnLists = {
 
 export const replaceBoldText = {
   pattern: /(On (Hit|Crit|Release|Channel):|Trigger:|Effect:|resistance to (that damage|Astral and Lunar|all damage|any damage from the attack)|resistances?|Discord|Unbreakable|immune|(difficulet|dangerous) terrain|(M|m)inor Action:|Summons?|hard cover|Armor|Stress|\d Stress|Enchantments?|bonus|Physical|Astral|Lunar|free action|critical hit|low visibility|Reroll Table|(Short )?Rests?|1\/turn|1\/round|Blast|Burst|Close Blast|Line|shift .|reactions?|Failure:|Success:|flight|Traps?|concentration save|Any.*:|Aquatic Adaptation|climb(ing)?|teleport|limit break|Reliable)/g,
-  replacer: (match, group) => {
+  replacer: (match: string, group: string) => {
     return `<span class="bold">${group}</span>`
   }
 }
 export const replaceAccuracy = {
   pattern: /\+(\d) Accuracy/g,
-  replacer: (match, group) => {
+  replacer: (match: string, group: string) => {
     return `<span class="bold">+${group}<span class="accuracy">+</span></span>`
   }
 }
 export const replaceDifficulty = {
   pattern: /\+(\d) Difficulty/g,
-  replacer: (match, group) => {
+  replacer: (match: string, group: string) => {
     return `<span class="bold">+${group}<span class="accuracy">-</span></span>`
   }
 }
 export const replaceSummonStatblock = {
   pattern: /(\(Size.*\))/g,
-  replacer: (match, group) => {
+  replacer: (match: string, group: string) => {
     return `<span class="literata-sc">${group}</span>`
   }
 }
 export const replacePills = {
   pattern: /\[([^\]]*)]/g,
-  replacer: (match, group) => {
+  replacer: (match: string, group: string) => {
     return `<span class="colored-pill">${group}</span>`
 
   }
@@ -64,20 +64,20 @@ export const replacePills = {
 
 export const replaceActionDescriptors = {
   pattern: /([A-Z]* \(\d\) [A-Z]*)/g,
-  replacer: (match, group) => {
+  replacer: (match: string, group: string) => {
     return `<span class="colored-text">${group}</span>`
   }
 }
 export const replacePhaseNumberInDescription = {
   pattern: /\((\d)\)/g,
-  replacer: (match, group) => {
+  replacer: (match: string, group: string) => {
     return `<span class="phase-number-descr">${group}</span>`
   }
 }
 
 export const replaceCapitalRulesPhrases = {
   pattern: /([A-Z-]{2,})/g,
-  replacer: (match, group) => {
+  replacer: (match: string, group: string) => {
     if (group.length > 2) {
       const firstLetter = group.substring(0, 1)
       const rest = group.substring(1)
