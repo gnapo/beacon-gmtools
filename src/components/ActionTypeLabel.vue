573cfb2f5158a85f8275d8bdf244f5af83ffcc7c
Fabian Stroh <fabian.stroh@gmail.com>
2024-12-21 19:24:36 +0100

    refactor text templating

--- src/components/ActionTypeLabel.vue
+++ src/components/ActionTypeLabel.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import {computed} from "vue";
+import {doReplacements, replacePhaseNumberInTop} from "@/util/textReplacement.ts";
 
 const props = defineProps<{actionText: string}>()
 
 const transformedContent = computed(() => {
-  return props.actionText.replace(/\((\d+)\)/g, (match, number) => {
-    return `<span class="phase-number">${number}</span>`;
-  });
+  return doReplacements(props.actionText, [replacePhaseNumberInTop])
 });
 </script>
 
---
+++ src/util/textReplacement.ts
@@ -0,0 +1,88 @@
+interface replacementAction {
+  pattern: RegExp
+  replacer: (match, p1: string) => string
+}
+
+
+export const doReplacements = (initialString: string, replacements: replacementAction[]): string => {
+  if (replacements.length === 0) {
+    return initialString
+  }
+  let modifiedString = initialString
+  replacements.forEach((replacement) => {
+    modifiedString = modifiedString.replace(replacement.pattern, replacement.replacer)
+  })
+  return modifiedString
+}
+
+export const replacePhaseNumberInTop = {
+  pattern: /\((\d+)\)/g,
+  replacer: (match, number) => {
+    return `<span class="phase-number">${number}</span>`
+  }
+}
+
+export const putLinebreaksOnLists = {
+  pattern: /\D (-) /g,
+  replacer: (match, group) => {
+    return ` <br> ✦ `
+  }
+}
+
+
+export const replaceBoldText = {
+  pattern: /(On (Hit|Crit|Release|Channel):|Trigger:|Effect:|resistance to (that damage|Astral and Lunar|all damage|any damage from the attack)|resistances?|Discord|Unbreakable|immune|(difficulet|dangerous) terrain|(M|m)inor Action:|Summons?|hard cover|Armor|Stress|\d Stress|Enchantments?|bonus|Physical|Astral|Lunar|free action|critical hit|low visibility|Reroll Table|(Short )?Rests?|1\/turn|1\/round|Blast|Burst|Close Blast|Line|shift .|reactions?|Failure:|Success:|flight|Traps?|concentration save|Any.*:|Aquatic Adaptation|climb(ing)?|teleport|limit break|Reliable)/g,
+  replacer: (match, group) => {
+    return `<span class="bold">${group}</span>`
+  }
+}
+export const replaceAccuracy = {
+  pattern: /\+(\d) Accuracy/g,
+  replacer: (match, group) => {
+    return `<span class="bold">+${group}<span class="accuracy">+</span></span>`
+  }
+}
+export const replaceDifficulty = {
+  pattern: /\+(\d) Difficulty/g,
+  replacer: (match, group) => {
+    return `<span class="bold">+${group}<span class="accuracy">-</span></span>`
+  }
+}
+export const replaceSummonStatblock = {
+  pattern: /(\(Size.*\))/g,
+  replacer: (match, group) => {
+    return `<span class="literata-sc">${group}</span>`
+  }
+}
+export const replacePills = {
+  pattern: /\[([^\]]*)]/g,
+  replacer: (match, group) => {
+    return `<span class="colored-pill">${group}</span>`
+
+  }
+}
+
+export const replaceActionDescriptors = {
+  pattern: /([A-Z]* \(\d\) [A-Z]*)/g,
+  replacer: (match, group) => {
+    return `<span class="colored-text">${group}</span>`
+  }
+}
+export const replacePhaseNumberInDescription = {
+  pattern: /\((\d)\)/g,
+  replacer: (match, group) => {
+    return `<span class="phase-number-descr">${group}</span>`
+  }
+}
+
+export const replaceCapitalRulesPhrases = {
+  pattern: /([A-Z-]{2,})/g,
+  replacer: (match, group) => {
+    if (group.length > 2) {
+      const firstLetter = group.substring(0, 1)
+      const rest = group.substring(1)
+      return `<span class="literata-sc"><span class="slightly-bigger">${firstLetter}</span>${rest}</span>`
+    }
+    return `<span class="literata-sc">${group}</span>`
+  }
+}
